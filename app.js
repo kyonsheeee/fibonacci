6c0679b28d7eb6f7d262c87c5c1a2668f457cff7
Kyoko Akatsuka <kyokoakatsuka38@gmail.com>
2021-09-16 11:06:06 +0900

    指数オーダーのfib関数の実装

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 'use strict';
+function fib(n) {
+    // 0 の時は 0 を返す
+    if (n === 0) {
+        return 0;
+    }
+    // 1 の時は 1 を返す
+    else if (n === 1) {
+        return 1;
+    }
+    // それ以外の時
+    return fib(n - 1) + fib(n - 2);
+}
+const length = 40;
+for (let i = 0; i <= length; i++){
+    console.log(fib(i));
+}
